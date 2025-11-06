2a74ae6e79ee52174e4d3c5ed70bf7db1c265e60
Maria Zakutey <joolveg@gmail.com>
2025-11-06 15:37:21 +0300

    new variant HomeWork04

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,6 +1,6 @@
 export default class ColumnChart {
   chartHeight = 50;
-  constructor ({ data = [], label = '', value = 0, link = '', formatHeading = data => data} = {}) {
+  constructor({ data = [], label = '', value = 0, link = '', formatHeading = data => data } = {}) {
     this.data = data;
     this.label = label;
     this.value = value;
@@ -11,26 +11,36 @@ export default class ColumnChart {
   }
 
   calcClass () {
-    return this.data?.length ? 'column-chart' : 'column-chart column-chart_loading';
+    return this.data.length ? 'column-chart' : 'column-chart column-chart_loading';
   }
 
   getTemplate () {
-    const linkTemplate = this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+    const link = this.link ? `<a href="${this.link}" class="column-chart__link">View all</a>` : '';
 
     return `
       <div class="column-chart__title">
         Total ${this.label}
-        ${this.link ? linkTemplate : ''}
+        ${link}
       </div>
       <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">
-          ${this.formatHeading(this.value)}
-        </div>
-        <div data-element="body" class="column-chart__chart">
-          ${this.getColumnChartBody()}
-        </div>
+        <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+        <div data-element="body" class="column-chart__chart">${this.getColumnChartBody()}</div>
       </div>
     `;
+  }
+
+  getColumnChartBody () {
+    if (!this.data.length) { return; }
+
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data.map(item => {
+      const percent = (item / maxValue * 100).toFixed(0);
+      const value = String(Math.floor(item * scale));
+
+      return `<div style="--value: ${value}" data-tooltip="${percent}%"></div>`;
+    }).join('');
   }
 
   render () {
@@ -41,18 +51,6 @@ export default class ColumnChart {
     this.element = wrapper;
   }
 
-  getColumnChartBody () {
-    const maxValue = Math.max(...this.data);
-
-    const scale = this.chartHeight / maxValue;
-
-    return this.data.map((item) => {
-      const percent = `${((item / maxValue) * 100).toFixed(0)}%`;
-
-      return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}"></div>`;
-    }).join('');
-  }
-
   update (newData = []) {
     this.data = newData;
     const newBodyChart = this.element.getElementsByClassName('column-chart__chart')[0];
@@ -61,10 +59,10 @@ export default class ColumnChart {
   }
 
   destroy () {
-    this.element.remove();
+    this.remove();
   }
 
   remove () {
-    this.element.parentElement.remove();
+    this.element.remove();
   }
 }
